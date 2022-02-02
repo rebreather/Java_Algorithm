92c5fb5816ef31556b405533bda9f8b1bb1b0c75
rebreather <yds04116@gmail.com>
2022-02-02 15:42:47 +0900

    220202 - lv1. 짝수와 홀수

---
+++ 프로그래머스 자바/lv1. 짝수와 홀수.java
@@ -0,0 +1,8 @@
+﻿class Solution {
+    public String solution(int num) {
+        String answer = "Even";
+        if(num % 2 == 0) return answer;
+        else if(num % 2 != 0) answer = "Odd";
+        return answer;
+    }
+}
